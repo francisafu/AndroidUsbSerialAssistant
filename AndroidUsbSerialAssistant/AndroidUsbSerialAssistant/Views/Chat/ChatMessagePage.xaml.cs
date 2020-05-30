383694623130e8b7e6092379bb1f108def7f459d
Francis Fu <francis_afu@163.com>
2020-05-30 21:44:31 +0800

    Add Chat Page and Update Nuget Packages

---
+++ AndroidUsbSerialAssistant/AndroidUsbSerialAssistant/Views/Chat/ChatMessagePage.xaml.cs
@@ -0,0 +1,34 @@
+using AndroidUsbSerialAssistant.Models.Chat;
+using Syncfusion.DataSource;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidUsbSerialAssistant.Views.Chat
+{
+    /// <summary>
+    /// Page to show chat message list
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatMessagePage :ContentPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChatMessagePage" /> class.
+        /// </summary>
+        public ChatMessagePage()
+        {
+            InitializeComponent();
+
+            ListView.DataSource.GroupDescriptors.Add(new GroupDescriptor
+            {
+                PropertyName = "Time",
+                KeySelector = obj =>
+                {
+                    var item = obj as ChatMessage;
+                    return item.Time.Date;
+                }
+            });
+        }
+    }
+}
