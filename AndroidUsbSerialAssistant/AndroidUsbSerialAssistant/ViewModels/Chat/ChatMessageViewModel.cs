457b5114d833c92182bcb64a6036adee8a738a0d
Francis Fu <francis_afu@163.com>
2020-05-31 19:06:21 +0800

    Remove Redundant Files and UI Adjust

--- AndroidUsbSerialAssistant/AndroidUsbSerialAssistant/ViewModels/Chat/ChatMessageViewModel.cs
+++ AndroidUsbSerialAssistant/AndroidUsbSerialAssistant/ViewModels/Chat/ChatMessageViewModel.cs
@@ -7,123 +7,100 @@ using Xamarin.Forms.Internals;
 namespace AndroidUsbSerialAssistant.ViewModels.Chat
 {
     /// <summary>
-    /// ViewModel for chat message page.
+    ///     ViewModel for chat message page.
     /// </summary>
     [Preserve(AllMembers = true)]
     public class ChatMessageViewModel : BaseViewModel
     {
+        #region Constructor
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ChatMessageViewModel" /> class.
+        /// </summary>
+        public ChatMessageViewModel()
+        {
+            MakeVoiceCall = new Command(VoiceCallClicked);
+            MakeVideoCall = new Command(VideoCallClicked);
+            MenuCommand = new Command(MenuClicked);
+            ShowCamera = new Command(CameraClicked);
+            SendAttachment = new Command(AttachmentClicked);
+            SendCommand = new Command(SendClicked);
+            BackCommand = new Command(BackButtonClicked);
+            ProfileCommand = new Command(ProfileClicked);
+
+            GenerateMessageInfo();
+        }
+
+        #endregion
+
         #region Fields
 
         /// <summary>
-        /// Stores the message text in an array. 
+        ///     Stores the message text in an array.
         /// </summary>
-        private readonly string[] descriptions = { "Hi, can you tell me the specifications of the Dell Inspiron 5370 laptop?",
-            " * Processor: Intel Core i5-8250U processor " +
-            "\n" + " * OS: Pre-loaded Windows 10 with lifetime validity" +
-            "\n" + " * Display: 13.3-inch FHD (1920 x 1080) LED display" +
-            "\n" + " * Memory: 8GB DDR RAM with Intel UHD 620 Graphics" +
-            "\n" + " * Battery: Lithium battery",
+        private readonly string[] descriptions =
+        {
+            "Hi, can you tell me the specifications of the Dell Inspiron 5370 laptop?",
+            " * Processor: Intel Core i5-8250U processor "
+            + "\n"
+            + " * OS: Pre-loaded Windows 10 with lifetime validity"
+            + "\n"
+            + " * Display: 13.3-inch FHD (1920 x 1080) LED display"
+            + "\n"
+            + " * Memory: 8GB DDR RAM with Intel UHD 620 Graphics"
+            + "\n"
+            + " * Battery: Lithium battery",
             "How much battery life does it have with wifi and without?",
-            "Approximately 5 hours with wifi. About 7 hours without.",
+            "Approximately 5 hours with wifi. About 7 hours without."
         };
 
         private string profileName = "Alex Russell";
 
         private string newMessage;
 
-        private string profileImage = "https://cdn.syncfusion.com/essential-ui-kit-for-xamarin.forms/common/uikitimages/ProfileImage3.png";
-
-        private ObservableCollection<ChatMessage> chatMessageInfo = new ObservableCollection<ChatMessage>();
-
-        #endregion
-
-        #region Constructor
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ChatMessageViewModel" /> class.
-        /// </summary>
-        public ChatMessageViewModel()
-        {
-            this.MakeVoiceCall = new Command(this.VoiceCallClicked);
-            this.MakeVideoCall = new Command(this.VideoCallClicked);
-            this.MenuCommand = new Command(this.MenuClicked);
-            this.ShowCamera = new Command(this.CameraClicked);
-            this.SendAttachment = new Command(this.AttachmentClicked);
-            this.SendCommand = new Command(this.SendClicked);
-            this.BackCommand = new Command(this.BackButtonClicked);
-            this.ProfileCommand = new Command(this.ProfileClicked);
-
-            this.GenerateMessageInfo();
-        }
+        private ObservableCollection<ChatMessage> chatMessageInfo =
+            new ObservableCollection<ChatMessage>();
 
         #endregion
 
         #region Public Properties
 
         /// <summary>
-        /// Gets or sets the profile name.
+        ///     Gets or sets the profile name.
         /// </summary>
         public string ProfileName
         {
-            get
-            {
-                return this.profileName;
-            }
-
+            get => profileName;
             set
             {
-                this.profileName = value;
-                this.NotifyPropertyChanged();
+                profileName = value;
+                NotifyPropertyChanged();
             }
         }
 
         /// <summary>
-        /// Gets or sets the profile image.
-        /// </summary>
-        public string ProfileImage
-        {
-            get
-            {
-                return this.profileImage;
-            }
-
-            set
-            {
-                this.profileImage = value;
-                this.NotifyPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets a collection of chat messages.
+        ///     Gets or sets a collection of chat messages.
         /// </summary>
         public ObservableCollection<ChatMessage> ChatMessageInfo
         {
-            get
-            {
-                return this.chatMessageInfo;
-            }
-
+            get => chatMessageInfo;
             set
             {
-                this.chatMessageInfo = value;
-                this.NotifyPropertyChanged();
+                chatMessageInfo = value;
+                NotifyPropertyChanged();
             }
         }
 
         /// <summary>
-        /// Gets or sets a new message.
+        ///     Gets or sets a new message.
         /// </summary>
         public string NewMessage
         {
-            get
-            {
-                return this.newMessage;
-            }
-
+            get => newMessage;
             set
             {
-                this.newMessage = value;
-                this.NotifyPropertyChanged();
+                newMessage = value;
+                NotifyPropertyChanged();
             }
         }
 
@@ -132,42 +109,42 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         #region Commands
 
         /// <summary>
-        /// Gets or sets the command that is executed when the profile name is clicked.
+        ///     Gets or sets the command that is executed when the profile name is clicked.
         /// </summary>
         public Command ProfileCommand { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the voice call button is clicked.
+        ///     Gets or sets the command that is executed when the voice call button is clicked.
         /// </summary>
         public Command MakeVoiceCall { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the video call button is clicked.
+        ///     Gets or sets the command that is executed when the video call button is clicked.
         /// </summary>
         public Command MakeVideoCall { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the menu button is clicked.
+        ///     Gets or sets the command that is executed when the menu button is clicked.
         /// </summary>
         public Command MenuCommand { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the camera button is clicked.
+        ///     Gets or sets the command that is executed when the camera button is clicked.
         /// </summary>
         public Command ShowCamera { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the attachment button is clicked.
+        ///     Gets or sets the command that is executed when the attachment button is clicked.
         /// </summary>
         public Command SendAttachment { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the send button is clicked.
+        ///     Gets or sets the command that is executed when the send button is clicked.
         /// </summary>
         public Command SendCommand { get; set; }
 
         /// <summary>
-        /// Gets or sets the command that is executed when the back button is clicked.
+        ///     Gets or sets the command that is executed when the back button is clicked.
         /// </summary>
         public Command BackCommand { get; set; }
 
@@ -175,46 +152,42 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
 
         #region Methods
 
-        /// <summary>base
-        /// Initializes a collection and add it to the message items.
+        /// <summary>
+        ///     base
+        ///     Initializes a collection and add it to the message items.
         /// </summary>
         private void GenerateMessageInfo()
         {
             var currentTime = DateTime.Now;
-            this.ChatMessageInfo = new ObservableCollection<ChatMessage>
+            ChatMessageInfo = new ObservableCollection<ChatMessage>
             {
                 new ChatMessage
                 {
-                    Message = this.descriptions[0],
+                    Message = descriptions[0],
                     Time = currentTime.AddMinutes(-2517),
-                    IsReceived = true,
+                    IsReceived = true
                 },
                 new ChatMessage
                 {
-                    Message = this.descriptions[1],
-                    Time = currentTime.AddMinutes(-2408),
+                    Message = descriptions[1],
+                    Time = currentTime.AddMinutes(-2408)
                 },
                 new ChatMessage
                 {
-                    ImagePath = "https://cdn.syncfusion.com/essential-ui-kit-for-xamarin.forms/common/uikitimages/Electronics.png",
-                    Time = currentTime.AddMinutes(-2405),
-                },
-                new ChatMessage
-                {
-                    Message = this.descriptions[2],
+                    Message = descriptions[2],
                     Time = currentTime.AddMinutes(-1103),
-                    IsReceived = true,
+                    IsReceived = true
                 },
                 new ChatMessage
                 {
-                    Message = this.descriptions[3],
-                    Time = currentTime.AddMinutes(-1006),
-                },
+                    Message = descriptions[3],
+                    Time = currentTime.AddMinutes(-1006)
+                }
             };
         }
 
         /// <summary>
-        /// Invoked when the voice call button is clicked.
+        ///     Invoked when the voice call button is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void VoiceCallClicked(object obj)
@@ -223,7 +196,7 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         }
 
         /// <summary>
-        /// Invoked when the video call button is clicked.
+        ///     Invoked when the video call button is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void VideoCallClicked(object obj)
@@ -232,7 +205,7 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         }
 
         /// <summary>
-        /// Invoked when the menu button is clicked.
+        ///     Invoked when the menu button is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void MenuClicked(object obj)
@@ -241,7 +214,7 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         }
 
         /// <summary>
-        /// Invoked when the camera icon is clicked.
+        ///     Invoked when the camera icon is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void CameraClicked(object obj)
@@ -250,7 +223,7 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         }
 
         /// <summary>
-        /// Invoked when the attachment icon is clicked.
+        ///     Invoked when the attachment icon is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void AttachmentClicked(object obj)
@@ -259,25 +232,22 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         }
 
         /// <summary>
-        /// Invoked when the send button is clicked.
+        ///     Invoked when the send button is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void SendClicked(object obj)
         {
-            if (!string.IsNullOrWhiteSpace(this.NewMessage))
-            {
-                this.ChatMessageInfo.Add(new ChatMessage
+            if (!string.IsNullOrWhiteSpace(NewMessage))
+                ChatMessageInfo.Add(new ChatMessage
                 {
-                    Message = this.NewMessage,
-                    Time = DateTime.Now
+                    Message = NewMessage, Time = DateTime.Now
                 });
-            }
 
-            this.NewMessage = null;
+            NewMessage = null;
         }
 
         /// <summary>
-        /// Invoked when the back button is clicked.
+        ///     Invoked when the back button is clicked.
         /// </summary>
         /// <param name="obj">The object</param>
         private void BackButtonClicked(object obj)
@@ -286,7 +256,7 @@ namespace AndroidUsbSerialAssistant.ViewModels.Chat
         }
 
         /// <summary>
-        /// Invoked when the Profile name is clicked.
+        ///     Invoked when the Profile name is clicked.
         /// </summary>
         private void ProfileClicked(object obj)
         {
--- AndroidUsbSerialAssistant/AndroidUsbSerialAssistant/Views/Chat/Selectors/MessageDataTemplateSelector.cs
+++ AndroidUsbSerialAssistant/AndroidUsbSerialAssistant/Views/Chat/Selectors/MessageDataTemplateSelector.cs
@@ -6,82 +6,54 @@ using Xamarin.Forms.Xaml;
 namespace AndroidUsbSerialAssistant.Views.Chat
 {
     /// <summary>
-    /// Implements the message data template selector class.
+    ///     Implements the message data template selector class.
     /// </summary>
     [Preserve(AllMembers = true)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public class MessageDataTemplateSelector : DataTemplateSelector
     {
         #region Constructor
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="MessageDataTemplateSelector" /> class.
+        ///     Initializes a new instance of the <see cref="MessageDataTemplateSelector" /> class.
         /// </summary>
         public MessageDataTemplateSelector()
         {
-            this.IncomingTextTemplate = new DataTemplate(typeof(IncomingTextTemplate));
-            this.OutgoingTextTemplate = new DataTemplate(typeof(OutgoingTextTemplate));
-            this.IncomingImageTemplate = new DataTemplate(typeof(IncomingImageTemplate));
-            this.OutgoingImageTemplate = new DataTemplate(typeof(OutgoingImageTemplate));
+            IncomingTextTemplate =
+                new DataTemplate(typeof(IncomingTextTemplate));
+            OutgoingTextTemplate =
+                new DataTemplate(typeof(OutgoingTextTemplate));
         }
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Gets or sets the incoming text template.
-        /// </summary>
-        public DataTemplate IncomingTextTemplate { get; set; }
-
-        /// <summary>
-        /// Gets or sets the outgoing text template.
-        /// </summary>
-        public DataTemplate OutgoingTextTemplate { get; set; }
-
-        /// <summary>
-        /// Gets or sets the incoming image template.
-        /// </summary>
-        public DataTemplate IncomingImageTemplate { get; set; }
-
-        /// <summary>
-        /// Gets or sets the outgoing text template.
-        /// </summary>
-        public DataTemplate OutgoingImageTemplate { get; set; }
 
         #endregion
 
         #region Methods
 
         /// <summary>
-        /// Returns the incoming or outgoing text template.
+        ///     Returns the incoming or outgoing text template.
         /// </summary>
         /// <param name="item">The item</param>
         /// <param name="container">The bindable object</param>
         /// <returns>Returns the data template</returns>
-        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        protected override DataTemplate OnSelectTemplate(object item,
+            BindableObject container)
         {
-            if (((ChatMessage)item).IsReceived)
-            {
-                if (string.IsNullOrEmpty(((ChatMessage)item).ImagePath))
-                {
-                    return this.IncomingTextTemplate;
-                }
-                else
-                {
-                    return this.IncomingImageTemplate;
-                }
-            }
-            else
-            {
-                if (string.IsNullOrEmpty(((ChatMessage)item).ImagePath))
-                {
-                    return this.OutgoingTextTemplate;
-                }
-                else
-                {
-                    return this.OutgoingImageTemplate;
-                }
-            }
+            return ((ChatMessage) item).IsReceived ? IncomingTextTemplate : OutgoingTextTemplate;
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the incoming text template.
+        /// </summary>
+        public DataTemplate IncomingTextTemplate { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the outgoing text template.
+        /// </summary>
+        public DataTemplate OutgoingTextTemplate { get; set; }
 
         #endregion
     }
